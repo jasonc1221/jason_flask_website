b4838baf34263c41a9a6932d7fc793da7e9f60e5
Jason Chang <jasonc1221@gmail.com>
2020-06-11 20:55:45 -0700

    add templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
+
+import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    age = int((datetime.date.today() - datetime.date(1996, 12, 21)).days / 365)
+    return render_template('index.html', title='Home', age=age)
